5aaf14439d2b777324aa7b7a6be3f9a1a9ef013a
Cyrille Djoko Djilo <cyrilledjoko@gmail.com>
2019-09-28 23:54:15 -0400

    adding state with useState hook

--- src/Counter.js
+++ src/Counter.js
@@ -1,16 +1,22 @@
-import React, {Fragment} from 'react';
+import React, { Fragment, useState } from 'react';
 
 const Counter = () => {
+    const [counter, setCounter] = useState({
+        count: 0,
+        name: 'djdj'
+    });
+
     function onClickHandler(event){
-        console.log('Button was pressed.', event);
+        console.log('Button was pressed.', event, counter);
+        setCounter({...counter, count: counter.count+1});
     }
     return (
         <Fragment>
-            <p></p>
+            <p>{counter.name}</p>
+            <p>{counter.count}</p>
             <button type="button" onClick={onClickHandler}>Increment</button>
         </Fragment>
     );
 }
-
 
 export default Counter;
