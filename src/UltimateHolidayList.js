0ff76979c62fd6617cdda8298fd79c4e4ed517ff
Cyrille Djoko <cyrilledjoko@gmail.com>
2019-11-03 17:12:55 -0500

    filtering collections client side with Array.filte r

--- src/UltimateHolidayList.js
+++ src/UltimateHolidayList.js
@@ -1,27 +1,28 @@
 import React, { useState, Fragment } from 'react';
 const list1 = [
-    { name: 'The beach' },
-    { name: 'The mountains' },
-    { name: 'Vibrant cities' },
-    { name: 'Roughing it' },
-    { name: 'Ultimate survival' },
+    { id: 1, topDestination: true, name: 'The beach' },
+    { id: 2, topDestination: false, name: 'The mountains' },
+    { id: 3, topDestination: true, name: 'Vibrant cities' },
+    { id: 4, topDestination: false, name: 'Roughing it' },
+    { id: 5, topDestination: false, name: 'Ultimate survival' },
 ], list2 = [
-    { name: 'The beach 2' },
-    { name: 'The mountains 2' },
-    { name: 'Vibrant cities 2' },
-    { name: 'Roughing it 2' },
-    { name: 'Ultimate survival 2' },
+    { id: 6, topDestination: true, name: 'The beach 2' },
+    { id: 7, topDestination: true, name: 'The mountains 2' },
+    { id: 8, topDestination: false, name: 'Vibrant cities 2' },
+    { id: 9, topDestination: false, name: 'Roughing it 2' },
+    { id: 10, topDestination: true, name: 'Ultimate survival 2' },
 ],
     UltimateHolidayList = () => {
         const [list, setList] = useState(list1);
+        const [showAll, setShowAll] = useState(true);
+
 
         const onClickHandler = () => setList(list === list1 ? list2 : list1);
-
-        const displayList = list.map((item, index) =>
-            <Fragment key={item.name}>
+        const displayList = list.filter(item => showAll || item.topDestination).map((item, index) =>
+            <Fragment key={item.id}>
                 <li>
                     <label htmlFor={`item${index}`}>{item.name}</label>
-                    <input id={`item${index}`} />
+                    {/* <input id={`item${index}`} /> */}
                 </li>
             </Fragment>
         )
@@ -31,7 +32,13 @@ const list1 = [
                 <h1>Ultimate Holiday Destinations</h1>
                 <ul>{displayList}</ul>
                 <button type="button" onClick={onClickHandler}>
-                    Change list
+                    Change list!
+                </button>
+                <button type="button" onClick={() => setShowAll(true)}>
+                    Show all!
+                </button>
+                <button type="button" onClick={() => setShowAll(false)}>
+                    Show only top destinations.
                 </button>
             </section>
         )
