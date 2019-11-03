2dbca94babf78100eb722b47053dd794d618cc4e
Cyrille Djoko <cyrilledjoko@gmail.com>
2019-11-03 17:53:21 -0500

    handling basic side effects

--- src/Counter.js
+++ src/Counter.js
@@ -1,17 +1,31 @@
-import React, { useState } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 import InteractiveView from './InteractiveView';
 
 const Counter = () => {
     const [counter, setCounter] = useState(0);
+    const [internalCount, setInternalCount] = useState(0);
 
-    function onIncrementHandler(){
+    useEffect(() => {
+        document.title = `counter set to ${counter} |Ultimate Counter`
+        console.log('title was set')
+    }, [counter])
+    function onIncrementHandler() {
         setCounter(c => ++c);
     }
 
+    function onInternalCountClickHandler() {
+        setInternalCount(c => c + 1);
+    }
+    console.log('main render')
     return (
-        <InteractiveView value={counter} onAction={onIncrementHandler}>
-            Increment
-        </InteractiveView>
+        <Fragment>
+            <InteractiveView value={counter} onAction={onIncrementHandler}>
+                Increment
+            </InteractiveView>
+            <button type="button" onClick={onInternalCountClickHandler}>
+                Increment Internal Count
+            </button>
+        </Fragment>
     );
 }
 
