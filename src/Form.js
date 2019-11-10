4093162c88fd199db6564af8a23840a3e1f66460
Cyrille Djoko Djilo <cyrilledjoko@gmail.com>
2019-11-09 19:18:19 -0500

    using a textarea

--- src/Form.js
+++ src/Form.js
@@ -2,8 +2,9 @@ import React, { useState } from 'react';
 import './form.scss';
 const initialState = {
         firstName: '',
-        lastName: ''
-    }
+        lastName: '',
+        biography: ''
+    };
 
 const Form = () => {
     const [formState, setFormState] = useState(initialState)
@@ -11,26 +12,29 @@ const Form = () => {
         setFormState({
             ...formState,
             [e.target.name]: e.target.value
-        })
+        });
     },
     onSubmitHandler = e => {
         console.log(formState, e);
         e.preventDefault();
     },
     onClickHandler = e => {
-        setFormState(initialState)
+        setFormState(initialState);
     }
     return (
         <form onSubmit={onSubmitHandler}>
             <span>Your name is {formState.firstName} {formState.lastName}</span>
+            <div>Biography: {formState.biography}</div>
             <label htmlFor="firstName">First Name</label>
             <input required id="firstName" name='firstName' onChange={onChangeHandler} value={formState.firstName} />
             <label htmlFor="lastName">Last Name</label>
             <input required id="lastName" name='lastName' onChange={onChangeHandler} value={formState.lastName} />
+            <label htmlFor="biography">Biography</label>
+            <textarea required rows='10' id="biography" name='biography' onChange={onChangeHandler} value={formState.biography} />
             <button type='submit'>Save</button>
             <button type="button" onClick={onClickHandler}>Clear values</button>
         </form>
-    )
+    );
 };
 
 export default Form;
