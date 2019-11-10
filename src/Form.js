40581329f57beae3a1930568e4a5a3d02c6db2d6
Cyrille Djoko Djilo <cyrilledjoko@gmail.com>
2019-11-09 19:25:23 -0500

    using a select

--- src/Form.js
+++ src/Form.js
@@ -3,7 +3,8 @@ import './form.scss';
 const initialState = {
         firstName: '',
         lastName: '',
-        biography: ''
+        biography: '',
+        transport: ''
     };
 
 const Form = () => {
@@ -25,12 +26,21 @@ const Form = () => {
         <form onSubmit={onSubmitHandler}>
             <span>Your name is {formState.firstName} {formState.lastName}</span>
             <div>Biography: {formState.biography}</div>
+            <div>Transport selected is {formState.transport}</div>
             <label htmlFor="firstName">First Name</label>
             <input required id="firstName" name='firstName' onChange={onChangeHandler} value={formState.firstName} />
             <label htmlFor="lastName">Last Name</label>
             <input required id="lastName" name='lastName' onChange={onChangeHandler} value={formState.lastName} />
             <label htmlFor="biography">Biography</label>
             <textarea required rows='10' id="biography" name='biography' onChange={onChangeHandler} value={formState.biography} />
+            <label htmlFor="transport">Transport Mode</label>
+            <select id="transport" name="transport" value={formState.transport} onChange={onChangeHandler}>
+                <option>None selected</option>
+                <option value="planes">Planes</option>
+                <option value="trains">Trains</option>
+                <option value="cars">Cars</option>
+                <option value="boats">Boats</option>
+            </select>
             <button type='submit'>Save</button>
             <button type="button" onClick={onClickHandler}>Clear values</button>
         </form>
