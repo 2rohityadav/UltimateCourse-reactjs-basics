37549ddc88aff4e40520e2f14ae3a03546c1cd4b
Cyrille Djoko <cyrilledjoko@gmail.com>
2019-11-16 13:10:02 -0500

    making routing accessible and hiding the initial outline

--- src/Section.js
+++ src/Section.js
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import Helmet from "react-helmet";
 
 const Section = ({ children, headingText, headingLevel = 2 }) => {
   const H = `h${headingLevel}`;
+  const headingRef = useRef(null);
+  useEffect(() => {
+    headingRef.current.focus();
+  }, []);
   return (
     <section>
       <Helmet>
         <title>{`${headingText} | Ultimate Weather Report`}</title>
       </Helmet>
-      <H>{headingText}</H>
+      <H ref={headingRef} tabIndex="-1" style={{ outline: "none" }}>
+        {headingText}
+      </H>
       {children}
     </section>
   );
